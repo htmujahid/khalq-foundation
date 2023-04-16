07d59c7e1384b460ef671379cf88029bfa0962d1
htmujahid <htmujahid@gmail.com>
2023-04-16 10:36:55 +0500

    default.png updated

--- resources/views/components/card/review.blade.php
+++ resources/views/components/card/review.blade.php
@@ -7,7 +7,7 @@
 @else
     <div class="p-6 sm:px-10 sm:py-8 rounded-3xl flex-shrink-0 bg-primary-light shadow w-5/6 sm:w-[448px]">
         <div class="flex gap-3 items-center">
-            <div class="w-12 h-12 bg-primary rounded-full"><img class="rounded-full" :src="$review->image" alt=""></div>
+            <div class="w-12 h-12 rounded-full"><img class="rounded-full" src="/assets/images/default.png" alt=""></div>
             <div><p class="font-medium text-2xl">{{$review->name}}</p></div>
         </div>
         <div class="mt-4">
