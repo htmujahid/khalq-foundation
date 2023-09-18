071019b61bba821bfe30a16033a6ed6901548346
Talha Mujahid <htmujahid@gmail.com>
2023-09-18 20:57:22 +0500

    Update team.blade.php

--- resources/views/components/card/team.blade.php
+++ resources/views/components/card/team.blade.php
@@ -1,7 +1,8 @@
 <x-card>
     <div class="flex items-center flex-col p-6 w-[290px]">
         <div class="max-w-[240px] h-[200px] rounded-lg mx-auto overflow-hidden">
-            <img src={{$member->image ?? "/assets/images/default.png"}}>
+            <img src={{"/assets/images/default.png"}}>
+<!--             <img src={{$member->image ?? "/assets/images/default.png"}}> -->
         </div>
         <div class="text-center">
             <p class="text-2xl font-medium pt-3">{{$member->name}}</p>
